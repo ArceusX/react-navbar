3e28ebd63530dbb4a7d99c72befe632b2f9637d7
Triet Lieu <trietmlieu@gmail.com>
2022-04-30 19:23:54 -0700

    Updates

---
+++ precache-manifest.8c40667d469ed5006c8d77177d4cecb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9fc561815b6339db5bed09931dc985f",
+    "url": "/react-navbar/index.html"
+  },
+  {
+    "revision": "aaa1dfef05305ef15be0",
+    "url": "/react-navbar/static/css/main.ab521689.chunk.css"
+  },
+  {
+    "revision": "66470911f2f010f72924",
+    "url": "/react-navbar/static/js/2.55db0001.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-navbar/static/js/2.55db0001.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaa1dfef05305ef15be0",
+    "url": "/react-navbar/static/js/main.939ad7a7.chunk.js"
+  },
+  {
+    "revision": "d068d03df11fcde21e97",
+    "url": "/react-navbar/static/js/runtime-main.315b024a.js"
+  }
+]);
